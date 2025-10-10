5ffe81c3eb5c66c7eae344acaccb423cc5eeb351
nassosliak <nassosliak@gmail.com>
2025-10-10 09:19:53 +0000

    Fix in trace driven

--- gpu-simulator/trace-driven/trace_driven.h
+++ gpu-simulator/trace-driven/trace_driven.h
@@ -190,7 +190,7 @@ class trace_shader_core_ctx : public shader_core_ctx {
  public:
   trace_shader_core_ctx(class gpgpu_sim *gpu, class simt_core_cluster *cluster,
                         unsigned shader_id, unsigned tpc_id,
-                        const shader_core_config *config,
+                        shader_core_config *config,
                         const memory_config *mem_config,
                         shader_core_stats *stats)
       : shader_core_ctx(gpu, cluster, shader_id, tpc_id, config, mem_config,
