d66a7a8b86f8e2679d9df6ff45c15cc460a7d5f4
nassosliak <nassosliak@gmail.com>
2025-10-20 17:34:54 +0000

    Reduced outputs

--- deadlock_sim_run_11.1/gaussian-rodinia-3.1/_s_16/QV100-Accelwattch_SASS_SIM/justrun.sh
+++
@@ -1,4 +0,0 @@
-export GPGPUSIM_CONFIG_PATH="/accel-sim-framework/gpu-simulator/gpgpu-sim/configs/tested-cfgs/SM7_QV100/configs/rodinia3.1/gaussian-rodinia-3.1/_s_16/gpgpusim.config"
-export GPGPUSIM_TRACE_CONFIG_PATH="/accel-sim-framework/gpu-simulator/configs/tested-cfgs/SM7_QV100/configs/rodinia3.1/gaussian-rodinia-3.1/_s_16/trace.config"
-export GPGPUSIM_RECONFIG_PATH="/accel-sim-framework/gpu-simulator/gpgpu-sim/configs/tested-cfgs/SM7_QV100/configs/rodinia3.1/gaussian-rodinia-3.1/_s_16/reconfig.txt"
- /accel-sim-framework/util/job_launching/../../sim_run_11.1/gpgpu-sim-builds/accelsim-commit-_modified_0.0_25-09-09-19-25-41gpgpu-sim_git-commit-d9e6e135_modified_3.0/accel-sim.out  -config ./gpgpusim.config -trace ./traces/kernelslist.g
